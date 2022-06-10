2c92f9780ba5c6cf297e77d447cff8b7f06274b4
ali alp <aliberk.alparslan@payporter.com.tr>
2022-06-10 16:00:59 +0300

    feign call added

--- account/src/main/java/com/aberk/account/service/AccountService.java
+++ account/src/main/java/com/aberk/account/service/AccountService.java
@@ -1,5 +1,7 @@
 package com.aberk.account.service;
 
+import com.aberk.account.feign.TransactionFeign;
+import com.aberk.account.feign.model.TransactionModel;
 import com.aberk.account.model.Account;
 import com.aberk.account.repository.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +15,9 @@ public class AccountService {
     @Autowired
     private AccountRepository accountRepository;
 
+    @Autowired
+    TransactionFeign transactionFeign;
+
     public Account findAccountById(int accountId) {
         return accountRepository.findById(accountId).orElse(null);
     }
@@ -24,10 +29,14 @@ public class AccountService {
     public Account addAccount(Account account){
         if(account.getBalance() != 0){
             try {
+                accountRepository.save(account);
+                TransactionModel initial_balance = new TransactionModel(account.getId(), account.getBalance(), "Initial Balance");
+                transactionFeign.createNewTransaction(initial_balance);
+
                 System.out.println("adding amount to accounts");
             }
             catch(Exception e){
-
+                System.out.println(e + "  error");
             }
         } else {
             System.out.println("adding 0 amount account");
